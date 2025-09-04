1b83879ce08633e240cde807e99c387d4aa50055
38257186sa-a11y <38257186sa@gmail.com>
2025-09-04 20:34:00 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,17 @@
+const questions = [
+  {
+    question: "Who is known as the father of Political Science?",
+    options: ["Plato", "Aristotle", "Rousseau", "Hobbes"],
+    answer: 1 // index of correct option (0 = first option)
+  },
+  {
+    question: "Who wrote 'Leviathan'?",
+    options: ["Locke", "Hobbes", "Rousseau", "Montesquieu"],
+    answer: 1
+  },
+  {
+    question: "Who gave the concept of 'General Will'?",
+    options: ["Hobbes", "Locke", "Rousseau", "Bentham"],
+    answer: 2
+  }
+];
